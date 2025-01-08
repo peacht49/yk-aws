9bb14f66dea44bb477543cad26392df08152f1a7
peacht <68547630+peacht49@users.noreply.github.com>
2025-01-08 21:29:23 +0800

    Delete HA-ansible-proxy directory

--- HA-ansible-proxy/modules/mariadb_asg/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "mariadb_launch_template" {
-  description = "Launch template for mariadb ASG"
-  value = aws_launch_template.mariadb_launch_template
-}
-
-output "mariadb_SG" {
-  description = "Security group for mariadb ASG"
-  value = aws_security_group.mariadb-sg.id
-}
